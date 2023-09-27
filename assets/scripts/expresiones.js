fd924bfa4d58d83b1f766085a92261875c393898
AlbertoSilvEn <aalbertosilvae@gmail.com>
2023-09-26 22:42:51 -0600

    Se aprende sobre operadores logicos y se ven variables

---
+++ assets/scripts/expresiones.js
@@ -0,0 +1,68 @@
+console.log("Estoy vivoooo")
+
+
+/*
+
+Operador
+Un elemento que nos permite rrealizar una operacion entre dos o mas elementos.
+
+-Operadores aritmeticos ( +, -, *, /)
+-Operadores de asignacion (=, ==, === / asignacion, igualdad, igualdad estricta)
+-Operadores de cadenas (tolwerCase, toUpperCase, trim, toString, conect)
+-Operadores logicos (&&, ||, !)
+
+*/
+
+/*OPERADORES ARITMETICOS
+
+Son operadores que nos permiten tomar valores numericos coo sus operandos y retornar un valor numerico unico. Corresponden operaciones matematicas.
+*/
+
+// Suma (+): s
+
+let resultado = 5+3; // resultado conrendrá el valor 8 
+
+// Resta (-):
+
+let resultado1 = 10 - 3; // resultado contendra el valor 6
+
+//Multiplicación (*) : se utiliza para multiplicar dos numeros.
+
+let resultado2 = 6 * 3; // resultado contendra el valor 18.
+
+// Division(/): Se utiliza para dividir un numero entre otro. La division por cero produce un valor especial llamado "infinity" (infinito).
+
+let resultado3 = 15 / 3; //Resultado contendra el valor 5
+
+// Modulo (%): Se utiliza para obtener el rsto de una division entre dos numeros.
+
+let resultado4 = 10 % 3; // resultado contendra el valor 1 (el resto de 10 dividido por 3)
+
+//Incremento (++): Se utiliza para aumentar en 1 el valor d euna variable.
+
+let numero = 5;
+numero ++
+// ahira numero contendra el valor 6
+
+// Decremento (--): se utiliza para disminuir en 1 el valor de una variable.+
+
+let numero1 = 8;
+numero--; //ahora el numero contendra el valor 7
+
+
+//Doble igual - igualdad abstracta
+5 == "5" // true, ya que JavaScript convierte la cadena "5" a un numero antes de la comparacion
+
+//Triple igual - igualdad estricta
+5 === "5" // false, por que  no son frl mismo tipo y los tipos tambien difieren.
+
+//------------
+
+//Ejercicio convertir grados centigrados a farenheit
+
+let gradosCentigrados = 15;
+
+let gradosFarenheit = ((9/5) * (gradosCentigrados) + 32);
+
+console.log(gradosFarenheit = ((9/5) * (gradosCentigrados) + 32))
+
